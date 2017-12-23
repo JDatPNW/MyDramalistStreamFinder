8a98a9d3fb8a9cf47cc7ca11bbe33e8446e1b9d9
jd^ <david.richter@freenet.de>
2017-12-23 01:17:16 +0100

    Improvements
    Added Dropdown, not functional yet

--- content.js
+++ content.js
@@ -39,11 +39,31 @@ window.addEventListener("load", function load(event) {
         return;
     }
 
-    var innerHTML = "<div class=\"btn-group group-manage-list dropdown m-b-sm btn-block\"><a href=\"#\" id=\"extension-link\" class=\"btn m-b-sm white btn-manage-list main col-xs-10 col-sm-9\">Stream</a></div>";
+
+    var innerHTML = "<div id=\"Steam_Buttons\" class=\"btn-group group-manage-list dropdown m-b-sm btn-block\"> ";
+    innerHTML += "<button id=\"Stream_Open_All_Links\" class=\"btn m-b-sm white btn-manage-list main col-xs-10 col-sm-9\" \">Stream Drama</button> ";
+    innerHTML += "<button id=\"Stream_Dropdown\" class=\"btn m-b-sm white btn-clist col-xs-2 col-sm-3\" data-toggle=\"dropdown\"><i class=\"fa fa-list\"></i></button> ";
+    innerHTML += "<div class=\"dropdown-menu dropdown-menu-right manage-clist\"> <div class=\"text-center p-a\">";
+    innerHTML += "<a id \"KA\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\" rel=\"nofollow\">KissAsia</a> ";
+    innerHTML += "<a class=\"dropdown-item m-t-sm m-b-sm btn-create-list\" rel=\"nofollow\"> Create a List</a>";
+    innerHTML += "<a class=\"dropdown-item m-t-sm m-b-sm btn-create-list\" rel=\"nofollow\"><i class=\"fa fa-plus\"></i> Create a List</a> </div> </div>";
+
+    /*
+    var innerHTML = "<div class=\"btn-group group-manage-list dropdown m-b-sm btn-block\"><a href=\"#\" ";
+    innerHTML += "id=\"extension-link\" class=\"btn m-b-sm white btn-manage-list main col-xs-10 col-sm-9\">Stream</a></div>";
+
+    innerHTML += " <button class=\"btn m-b-sm white btn-clist col-xs-2 col-sm-3\" data-toggle=\"dropdown\"><i class=\"fa fa-list\"></i></button> <div data-rid=\"23950\" class=\"dropdown-menu dropdown-menu-right manage-clist\"> <div class=\"text-center p-a\"><i class=\"fa fa-spin fa-spinner\"></i></div> <div class=\"dropdown-divider\"></div> <a class=\"dropdown-item m-t-sm m-b-sm btn-create-list\" rel=\"nofollow\"><i class=\"fa fa-plus\"></i> Create a List</a> </div> </div>"
+*/
     elements[0].appendChild(createElement(innerHTML));
 
-    var link = document.getElementById('extension-link');
+    var link = document.getElementById('Stream_Open_All_Links');
     link.addEventListener('click', function() {
         openStreamTabs();
     });
+
+    var link = document.getElementById('KA');
+    link.addEventListener('click', function() {
+        openStreamTabs();
+    });
+
 }, false);
