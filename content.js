ab59fe3a87b53a4c7ab2804c3a4e071088ae8c0c
David <david.richter@freenet.de>
2020-12-13 12:41:35 -0600

    updated to work with netflix, viki and so on

--- content.js
+++ content.js
@@ -4,33 +4,35 @@ function openInNewTab(url) {
 }
 
 function openStreamTabs(Selection){
-    var url = window.location.href;
 
-    var partsArray = url.split('-');
-    var KA_URL = "http://www.kissasian.ch/drama/"; //KissAsian
-    var DC_URL = "https://dramacool9.com/drama-detail/" //DramaCool
-    var DN_URL = "https://ondramanice.io/drama/" //DramaNice
-    var GDJ_URL = "http://www.gooddrama.to/japanese-drama/" //GoodDrama_JP
+    var name = document.getElementsByClassName("film-title")[0].textContent;
+    var partsArray = name.split(' ');
+    var VK_URL = "https://www.viki.com/search?q=";
+    var NF_URL = "https://www.netflix.com/search?q=";
+    var AP_URL = "https://www.amazon.com/s?k=";
+    var AP_PostFix = "&i=prime-instant-video&ref=nb_sb_noss_2";
+    var KK_URL = "https://www.kocowa.com/en_us/search/";
 
-    for (var i = 1; i < partsArray.length; i++){
-        KA_URL = KA_URL + partsArray[i] + "-";
-        DC_URL = DC_URL + partsArray[i] + "-";
-        DN_URL = DN_URL + partsArray[i] + "-";
-        GDJ_URL = GDJ_URL + partsArray[i] + "-";
+    for (var i = 0; i < partsArray.length - 1; i++){
+        VK_URL = VK_URL + partsArray[i] + "%20";
+        NF_URL = NF_URL + partsArray[i] + "%20";
+        AP_URL = AP_URL + partsArray[i] + "+";
+        KK_URL = KK_URL + partsArray[i] + "%20";
     }
-    KA_URL = KA_URL.substring(0, KA_URL.length - 1);
-    DC_URL = DC_URL.substring(0, DC_URL.length - 1);
-    GDJ_URL = GDJ_URL.substring(0, GDJ_URL.length - 1);
-    DN_URL += "detail";
+    VK_URL = VK_URL.substring(0, VK_URL.length - 3);
+    NF_URL = NF_URL.substring(0, NF_URL.length - 3);
+    AP_URL = AP_URL.substring(0, AP_URL.length - 1);
+    AP_URL = AP_URL + AP_PostFix;
+    KK_URL = KK_URL.substring(0, KK_URL.length - 3);
 
-    if (Selection == "Select_All" || Selection == "Select_KA")
-      openInNewTab(KA_URL);
-    if (Selection == "Select_All" || Selection == "Select_DC")
-      openInNewTab(DC_URL);
-    if (Selection == "Select_All" || Selection == "Select_DN")
-      openInNewTab(DN_URL);
-    if (Selection == "Select_All" || Selection == "Select_GDJ")
-      openInNewTab(GDJ_URL);
+    if (Selection == "Select_VK")
+      openInNewTab(VK_URL);
+    if (Selection == "Select_NF")
+      openInNewTab(NF_URL);
+    if (Selection == "Select_AP")
+      openInNewTab(AP_URL);
+    if (Selection == "Select_KK")
+      openInNewTab(KK_URL);
 }
 
 function createElement( str ) {
@@ -54,42 +56,40 @@ window.addEventListener("load", function load(event) {
       }
 
       var innerHTML = "<div id=\"Stream_Buttons\" style=\"margin-top: -.5rem!important\" class=\"btn-group group-manage-list dropdown m-b-sm btn-block\"> ";
-      innerHTML    += "<button id=\"Stream_Open_All_Links\" class=\"btn white btn-manage-list main col-xs-10 col-sm-9\" \">Stream Drama</button> ";
+      innerHTML    += "<button id=\"Stream_Open_All_Links\" class=\"btn white main col-xs-10 col-sm-9\" data-toggle=\"dropdown\">Stream Drama</button> ";
       innerHTML    += "<button id=\"Stream_Dropdown\" class=\"btn white btn-clist col-xs-2 col-sm-3\" data-toggle=\"dropdown\"><i class=\"fa fa-list\"></i></button> ";
       innerHTML    += "<div class=\"dropdown-menu dropdown-menu-right manage-clist\">";
       innerHTML    += "<div class=\"text-center p-a\">";
-      innerHTML    += "<button id =\"KA_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\">KissAsian</button> ";
-      innerHTML    += "<button id =\"DC_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\">DramaCool</a>";
-      innerHTML    += "<button id =\"DN_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\"</i>DramaNice</a>";
-      innerHTML    += "<button id =\"GDJ_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\"</i>GoodDrama Japanese</a>";
+      innerHTML    += "<button id =\"VK_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\">Viki</button> ";
+      innerHTML    += "<button id =\"NF_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\">Netflix</a>";
+
+      innerHTML    += "<button id =\"AP_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\"</i>Amazon Prime</a>";
+      innerHTML    += "<button id =\"KK_Dropdown\" class=\"dropdown-item m-t-sm m-b-sm btn-create-list\"</i>Kokowa</a>";
       innerHTML    += "</div> </div>";
 
       elements[0].insertBefore(createElement(innerHTML), elements[0].childNodes[elements[0].childNodes.length - 2]);
       //elements[0].appendChild(createElement(innerHTML));
 
-      var All_Links = document.getElementById('Stream_Open_All_Links');
-      All_Links.addEventListener('click', function() {
-          openStreamTabs("Select_All");
+
+      var VK_Link = document.getElementById('VK_Dropdown');
+      VK_Link.addEventListener('click', function() {
+          openStreamTabs("Select_VK");
       });
 
-      var KA_Link = document.getElementById('KA_Dropdown');
-      KA_Link.addEventListener('click', function() {
-          openStreamTabs("Select_KA");
+      var NF_Link = document.getElementById('NF_Dropdown');
+      NF_Link.addEventListener('click', function() {
+          openStreamTabs("Select_NF");
       });
 
-      var DC_Link = document.getElementById('DC_Dropdown');
-      DC_Link.addEventListener('click', function() {
-          openStreamTabs("Select_DC");
+
+      var AP_Link = document.getElementById('AP_Dropdown');
+      AP_Link.addEventListener('click', function() {
+          openStreamTabs("Select_AP");
       });
 
-      var DN_Link = document.getElementById('DN_Dropdown');
-      DN_Link.addEventListener('click', function() {
-          openStreamTabs("Select_DN");
-      });
-
-      var GDJ_Link = document.getElementById('GDJ_Dropdown');
-      GDJ_Link.addEventListener('click', function() {
-          openStreamTabs("Select_GDJ");
+      var KK_Link = document.getElementById('KK_Dropdown');
+      KK_Link.addEventListener('click', function() {
+          openStreamTabs("Select_KK");
       });
 };
 }, false);
